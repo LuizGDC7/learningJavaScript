d2229d5f0aa0c1daadf8e61013919138509c6786
LuizGDC7 <guilhermedekelly@gmail.com>
2025-02-22 21:00:40 -0300

    [Feature] adição de conhecimento ao anotacoes.txt

---
+++ curso_js/js/eventos.js
@@ -0,0 +1,14 @@
+document.getElementById('click-me').onclick = function () {
+        alert("click")
+    } 
+
+
+document.getElementById('hover-me').onmouseover = function () {
+        alert("Hover")
+    } 
+
+
+document.getElementById('leave-me').onmouseleave = 
+    function () {
+        alert("Saiu")
+    } 
---
+++ curso_js/js/objetos.js
@@ -0,0 +1,10 @@
+var aluno = {
+    'nome': "Luiz",
+    'idade': 21,
+    'getNome': function(){
+        return this.nome
+    }
+}
+
+console.log(aluno.getNome())
+
